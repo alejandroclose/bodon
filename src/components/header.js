77f81be17495a97d07ad97d636b69b7500a4da00
Alejandro Close <alejandroclose@Alejandros-MacBook-Pro.local>
2020-03-18 19:06:25 +0100

    added hamburger menu to mobile view

--- src/components/header.js
+++ src/components/header.js
@@ -8,18 +8,23 @@ class Header extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      headerIsOpen: true
+      headerIsOpen: false,
     };
   }
+
+  handleClick = () => {
+    this.setState({ headerIsOpen: !this.state.headerIsOpen })
+  }
+
   render() {
     return (
       <header>
         <div className="header-content">
           <div className="header-brand">
             <Link to="/"><Logo className="header-logo" /></Link>
-            <button className={this.state.headerIsOpen ? "header-btn" : "hidden"}>V</button>
+            <button className="header-btn" onClick={this.handleClick.bind(this)}>Menu</button>
           </div>
-          <div className="header-menu">
+          <div className={this.state.headerIsOpen ? "header-menu" : "header-menu hidden"}>
             <Link to="#evento">EL EVENTO</Link>
             <Link to="#confirmar">CONFIRMAR</Link>
             <Link to="#confirmar">DRESS CODE</Link>
