24c09cb902e91529c4090cb6f53ade670d53b944
Alejandro Close <alejandroclose@Alejandros-MacBook-Pro.local>
2020-03-30 23:33:27 +0200

    graphql

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -1,23 +1,23 @@
 const { ApolloServer, gql } = require('apollo-server-lambda');
- 
+
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
     hello: String
   }
 `;
- 
+
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
   },
 };
- 
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  
+
   // By default, the GraphQL Playground interface and GraphQL introspection
   // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
   //
@@ -26,5 +26,5 @@ const server = new ApolloServer({
   playground: true,
   introspection: true,
 });
- 
+
 exports.handler = server.createHandler();
