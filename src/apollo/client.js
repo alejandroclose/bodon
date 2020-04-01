20da86cae2871fff1948d56a916cd9f985da7c30
Alejandro Close <alejandroclose@Alejandros-MacBook-Pro.local>
2020-04-02 01:33:45 +0200

    wip gql test 2

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -3,5 +3,9 @@ import fetch from 'isomorphic-fetch';
 
 export const client = new ApolloClient({
   uri: 'https://alejandrayalejandro.com/.netlify/functions/graphql',
+  onError: ({ networkError, graphQLErrors }) => {
+    console.log('graphQLErrors', graphQLErrors)
+    console.log('networkError', networkError)
+  },
   fetch
 })
