d026366f514c3c10827efd0fd8c9eea39c401c8d
Alejandro Close <alejandroclose@Alejandros-MacBook-Pro.local>
2020-04-02 01:18:22 +0200

    updated cors

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -70,4 +70,9 @@ const server = new ApolloServer({
   introspection: true,
 });
 
-exports.handler = server.createHandler();
+exports.handler = server.createHandler({
+  cors: {
+    origin: "*",
+    credentials: true
+  }
+});
